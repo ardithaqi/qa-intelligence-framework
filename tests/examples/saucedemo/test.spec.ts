2d3c4ff4c09dcc30073b8e67812820350c209e84
ardi-macbook <aardithaqi@gmail.com>
2026-02-27 18:53:05 +0100

    changed import path

--- tests/examples/saucedemo/test.spec.ts
+++ tests/examples/saucedemo/test.spec.ts
@@ -1,4 +1,5 @@
-import { test, expect } from "@playwright/test";
+import { test } from "../../../src/core/testHooks";
+import { expect } from "@playwright/test";
 
 test("forced regression", async ({ page }) => {
     await page.goto("https://www.saucedemo.com");
